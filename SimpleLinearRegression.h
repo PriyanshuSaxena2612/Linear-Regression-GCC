ca09aef75deddc9314a7cddf9b989787e804e14e
Priyanshu <priyanshusaxena2612@gmail.com>
2021-08-11 01:22:26 +0530

    All methods included

--- SimpleLinearRegression.h
+++ SimpleLinearRegression.h
@@ -9,17 +9,38 @@ using namespace std;
 
 namespace linearRegressionMaths
 {
-    // method to calculate mean.
+    double M = 0;
+    double C = 0;
+    /*
+        method to calculate mean.
+    */
     template <typename T>
     double mean(vector<T>);
-    // method to calculate vector multiplication element by element
+    /*
+        method to calculate vector multiplication element by element
+    */
     template <typename T>
     vector<T> vectorMultiply(vector<T> &vector1, vector<T> &vector2);
-    // method to calculate m and c for the equation y = mx + c.
+    /*
+        Method to calculate slope of y = mx + c
+    */
     template <typename T>
-    double calculateSlope(vector<T> &X, vector<T> &Y);
+    double calculateSlope(vector<T> &X, vector<T> &Y, double &M);
+    /*
+        Method to calculate intercept of y = mx + c
+    */
     template <typename T>
-    double calculateIntercept(vector<T> &X, vector<T> &Y);
+    double calculateIntercept(vector<T> &X, vector<T> &Y, double &C);
+    /*
+        Method to do predictions
+    */
+    template <typename T>
+    vector<double> predict(vector<T> &testData, double &M, double &C);
+    /*
+        Method to calculate score
+    */
+    template <typename T>
+    double score(vector<T> &truth, vector<T> &predicted);
 }
 
 template <typename T>
@@ -44,17 +65,49 @@ vector<T> linearRegressionMaths::vectorMultiply(vector<T> &vector1, vector<T> &v
 }
 
 template <typename T>
-double linearRegressionMaths::calculateSlope(vector<T> &X, vector<T> &Y)
+double linearRegressionMaths::calculateSlope(vector<T> &X, vector<T> &Y, double &M)
 {
     double numerator = mean(vectorMultiply(X, Y)) - (mean(X) * mean(Y));
     double denominator = mean(vectorMultiply(X, X)) - (mean(X) * mean(X));
-    return (numerator / denominator);
+    M = (numerator / denominator);
+    return M;
 }
 
 template <typename T>
-double linearRegressionMaths::calculateIntercept(vector<T> &X, vector<T> &Y)
+double linearRegressionMaths::calculateIntercept(vector<T> &X, vector<T> &Y, double &C)
 {
-    double slope = calculateSlope(X, Y);
+    double slope = calculateSlope(X, Y, M);
     double intercept = mean(Y) - slope * mean(X);
-    return intercept;
+    C = intercept;
+    return C;
+}
+
+template <typename T>
+vector<double> linearRegressionMaths::predict(vector<T> &testData, double &M, double &C)
+{
+    vector<double> temp(testData.size());
+    if (testData.size())
+    {
+        for (T i = 0; i < testData.size(); i++)
+        {
+            // cout << M * testData[i] + C << " ";
+            temp[i] = M * testData[i] + C;
+        }
+    }
+    return temp;
+}
+
+template <typename T>
+double linearRegressionMaths::score(vector<T> &truth, vector<T> &predicted)
+{
+    double u, v;
+    if (truth.size() == predicted.size() && truth.size() != 0)
+    {
+        for (T i = 0; i < truth.size(); i++)
+        {
+            u += (truth[i] - predicted[i]) * (truth[i] - predicted[i]);
+            v += (truth[i] - mean(truth)) * (truth[i] - mean(truth));
+        }
+    }
+    return (1 - u / v);
 }
--- main.cpp
+++ main.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
-#include <matplotlibcpp.h>
 #include "SimpleLinearRegression.h"
-// #include <vector>
-// #include <algorithm>
-
-// using namespace std;
-namespace plt = matplotlibcpp;
 using namespace linearRegressionMaths;
 
 int main()
 {
-    // testing the methods from the custom made library.
-    vector<int> v1 = {1, 2, 3, 4, 5};
-    vector<int> v2 = {4, 5, 6, 7, 8};
-    vector<int> temp = vectorMultiply(v1, v2);
-    // testing the mean function
+    double m = 0, c = 0;
+    /*
+        testing the methods from the custom made library
+    */
+    vector<double> v1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<double> v2 = {1, 3, 2, 5, 7, 8, 8, 9, 10, 12};
+    vector<double> temp1 = vectorMultiply(v1, v2);
+    /*
+        testing the mean function
+    */
     cout << "Testing mean function from SimpleLinearRegression.h: \n";
     cout << "Mean of vector v1 is: " << mean(v1) << "\n";
-    // testing the vector multiplication
+    /*
+        testing the vector multiplication
+    */
     cout << "Vector multiplication element by element of v1 and v2 is: \n";
-    for (auto i : temp)
+    for (auto i : temp1)
     {
         cout << i << " ";
     }
-    // testing the slope method
-    cout << "\nSlope of v1 and v2 together is: " << calculateSlope(v1, v2);
-    cout << "\nIntercept of v1 and v2 together is: " << calculateIntercept(v1, v2);
+    /*
+        testing the slope method
+    */
+    cout << "\nSlope of v1 and v2 together is: " << calculateSlope(v1, v2, m);
+    /*
+        testing the intercept method
+    */
+    cout << "\nIntercept of v1 and v2 together is: " << calculateIntercept(v1, v2, c);
+    /*
+        calculating the values of m and c
+    */
+    cout << "\nm and c are: " << m << " " << c << "\n";
+    /*
+        Testing the Prediction method
+    */
+    vector<double> v1_prediction = predict(v1, m, c);
+    cout << "The predicted values for v1 are:\n";
+    for (auto i : v1_prediction)
+    {
+        cout << i << " ";
+    }
+    /*
+        Testing the score
+    */
+    cout << "\nThe score of v1 and v2 is: " << score(v2, v1_prediction);
 }
