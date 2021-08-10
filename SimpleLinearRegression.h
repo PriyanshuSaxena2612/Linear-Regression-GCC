1fa240cb46373d648b3aa30f393df2136722f851
Priyanshu <priyanshusaxena2612@gmail.com>
2021-08-10 18:19:19 +0530

    First Commit

---
+++ SimpleLinearRegression.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+// namespace for containing all the methods in it.
+
+namespace linearRegressionMaths
+{
+    // method to calculate mean.
+    template <typename T>
+    T mean(vector<T>);
+    // method to calculate vector multiplication element by element
+    template <typename T>
+    vector<T> vectorMultiply(vector<T> &vector1, vector<T> &vector2);
+    // method to calculate m and c for the equation y = mx + c.
+    template <typename T, typename M>
+    void calculateCoef(vector<T> &X, vector<T> &Y, M &m, M &c);
+}
+
+template <typename T>
+T linearRegressionMaths::mean(vector<T> data)
+{
+    T mean = std::accumulate(data.begin(), data.end()) / data.size();
+    return mean;
+}
