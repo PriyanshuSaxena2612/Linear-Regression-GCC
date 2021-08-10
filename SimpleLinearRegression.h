4f52b15f68fc282332f1075192059dc593b2cd72
Priyanshu <priyanshusaxena2612@gmail.com>
2021-08-10 19:48:28 +0530

    Matplotlib added

--- SimpleLinearRegression.h
+++ SimpleLinearRegression.h
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -12,18 +11,50 @@ namespace linearRegressionMaths
 {
     // method to calculate mean.
     template <typename T>
-    T mean(vector<T>);
+    double mean(vector<T>);
     // method to calculate vector multiplication element by element
     template <typename T>
     vector<T> vectorMultiply(vector<T> &vector1, vector<T> &vector2);
     // method to calculate m and c for the equation y = mx + c.
-    template <typename T, typename M>
-    void calculateCoef(vector<T> &X, vector<T> &Y, M &m, M &c);
+    template <typename T>
+    double calculateSlope(vector<T> &X, vector<T> &Y);
+    template <typename T>
+    double calculateIntercept(vector<T> &X, vector<T> &Y);
 }
 
 template <typename T>
-T linearRegressionMaths::mean(vector<T> data)
+double linearRegressionMaths::mean(vector<T> data)
 {
-    T mean = std::accumulate(data.begin(), data.end(), 0.0) / data.size();
+    double mean = std::accumulate(data.begin(), data.end(), 0.0) / data.size();
     return mean;
+}
+
+template <typename T>
+vector<T> linearRegressionMaths::vectorMultiply(vector<T> &vector1, vector<T> &vector2)
+{
+    vector<T> temp(vector1.size());
+    if (vector1.size() == vector2.size() && vector1.size() != 0)
+    {
+        for (T i = 0; i < vector1.size(); i++)
+        {
+            temp[i] = (vector1[i] * vector2[i]);
+        }
+    }
+    return temp;
+}
+
+template <typename T>
+double linearRegressionMaths::calculateSlope(vector<T> &X, vector<T> &Y)
+{
+    double numerator = mean(vectorMultiply(X, Y)) - (mean(X) * mean(Y));
+    double denominator = mean(vectorMultiply(X, X)) - (mean(X) * mean(X));
+    return (numerator / denominator);
+}
+
+template <typename T>
+double linearRegressionMaths::calculateIntercept(vector<T> &X, vector<T> &Y)
+{
+    double slope = calculateSlope(X, Y);
+    double intercept = mean(Y) - slope * mean(X);
+    return intercept;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
+#include <matplotlibcpp.h>
 #include "SimpleLinearRegression.h"
-#include <vector>
-#include <algorithm>
+// #include <vector>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
+namespace plt = matplotlibcpp;
 using namespace linearRegressionMaths;
 
 int main()
 {
     // testing the methods from the custom made library.
-    vector<int> v = {1, 2, 3, 4, 5};
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    vector<int> v2 = {4, 5, 6, 7, 8};
+    vector<int> temp = vectorMultiply(v1, v2);
+    // testing the mean function
     cout << "Testing mean function from SimpleLinearRegression.h: \n";
-    cout << "Mean of vector v is: " << mean(v) << "\n";
+    cout << "Mean of vector v1 is: " << mean(v1) << "\n";
+    // testing the vector multiplication
+    cout << "Vector multiplication element by element of v1 and v2 is: \n";
+    for (auto i : temp)
+    {
+        cout << i << " ";
+    }
+    // testing the slope method
+    cout << "\nSlope of v1 and v2 together is: " << calculateSlope(v1, v2);
+    cout << "\nIntercept of v1 and v2 together is: " << calculateIntercept(v1, v2);
 }
